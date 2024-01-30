1064a363108587db9343ed776abe1f7161c49c33
abiyyu03 <abiyyu652@gmail.com>
2024-01-30 10:05:56 +0700

    implement login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,79 +1,56 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
+    <div class="container-full h-screen flex items-center bg-main">
+        <div class="shadow rounded-xl mx-auto bg-background">
+            <div class="flex p-8 max-w-2xl">
+                <div class="mr-4 max-auto block">
+                    <h2 class="text-center text-3xl font-bold text-secondary">KPR STT-NF 2024</h2>
+                    <div class="">
+                        <img src="{{ asset('img/logo.png') }}" alt="pemira_logo" width="170" class="mx-auto" />
+                    </div>
+                    <p class="text-center text-secondary">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur quae commodi quibusdam
+                        optio non aspernatur esse ratione dolorum maiores quisquam corrupti nobis sapiente pariatur
+                        ducimus deleniti dicta, deserunt molestias dolore!
+                    </p>
+                </div>
+                <div class="shadow rounded-xl p-5 bg-card items-center">
+                    <h1 class="text-center text-2xl mt-10">Login</h1>
                     <form method="POST" action="{{ route('auth') }}">
                         @csrf
+                        <div class="mb-3">
+                            <label for="nim">NIM</label>
+                            <input id="nim" type="nim"
+                                class="border-2 block border-secondary p-2 rounded-lg @error('nim') is-invalid @enderror"
+                                name="nim" value="{{ old('nim') }}" required autocomplete="nim" autofocus>
 
-                        <div class="row mb-3">
-                            <label for="nim" class="col-md-4 col-form-label text-md-end">{{ __('NIM')
-                                }}</label>
-
-                            <div class="col-md-6">
-                                <input id="nim" type="nim" class="form-control @error('nim') is-invalid @enderror"
-                                    name="nim" value="{{ old('nim') }}" required autocomplete="nim" autofocus>
-
-                                @error('nim')
+                            @error('nim')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
-                                @enderror
-                            </div>
+                            @enderror
                         </div>
+                        <div class="mb-3">
+                            <label for="">Password</label>
+                            <input id="password" type="password"
+                                class="border-2 block border-secondary p-2 rounded-lg @error('password') is-invalid @enderror"
+                                name="password" required autocomplete="current-password">
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password')
-                                }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password"
-                                    class="form-control @error('password') is-invalid @enderror" name="password"
-                                    required autocomplete="current-password">
-
-                                @error('password')
+                            @error('password')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
-                                @enderror
-                            </div>
+                            @enderror
                         </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{
-                                        old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                                @endif
-                            </div>
+                        <div class="mt-5">
+                            <button type="submit" class="p-3 bg-main text-white hover:bg-blue-400 rounded w-full">
+                                Login
+                            </button>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
