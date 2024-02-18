94bb2c73dc0dc8b91f902590bf009b81b9d48f67
abiyyu03 <abiyyu652@gmail.com>
2024-02-18 16:56:43 +0700

    Co-authored-by: Muhammad Asnur Ramdani <asnur@users.noreply.github.com>

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -23,6 +23,6 @@ class AdminSeeder extends Seeder
             'password' => bcrypt('kprsttnf!@#'),
         ]);
 
-        $user->assignRole('admin');
+        $user->assignRole('Admin');
     }
 }
