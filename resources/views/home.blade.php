32106bd392a15a5ebee2360d1922f2dc1ed3b26c
abiyyu03 <abiyyu652@gmail.com>
2024-02-18 15:20:01 +0700

    fix error tailwind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,8 @@
         integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>#yukVote | Pemilihan Raya STTNF 2024</title>
-    @vite('resources/css/app.css')
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <link href="https://fonts.googleapis.com/css?family=Inter" rel="stylesheet" />
     {{-- <link href="https://cdn.jsdelivr.net/npm/daisyui@4.5.0/dist/full.min.css" rel="stylesheet" type="text/css" /> --}}
     <style>
