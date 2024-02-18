9e70829356b423cce1aae990567332d8ad553be4
abiyyu03 <abiyyu652@gmail.com>
2024-02-18 23:21:33 +0700

    fix resources call in vote layout

--- resources/views/layouts/vote.blade.php
+++ resources/views/layouts/vote.blade.php
@@ -3,9 +3,9 @@
 
 <head>
     <meta charset="UTF-8" />
-    <title>Vote BEM - Pemira 2024 STTNF</title>
+    <title>Yuk Vote - Pemira 2024 STTNF</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    @vite('resources/css/app.css')
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <link href="https://cdn.jsdelivr.net/npm/daisyui@4.5.0/dist/full.min.css" rel="stylesheet" type="text/css" />
 </head>
 
