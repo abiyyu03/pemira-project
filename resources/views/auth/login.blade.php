d8a14baf2aeeaaf74ae2c53afefb28d9cad0065a
abiyyu03 <abiyyu652@gmail.com>
2024-02-19 13:45:28 +0700

    fix responsive login and ready to vote

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     <div class="container-full h-screen flex items-center bg-main">
         <div class="shadow rounded-xl mx-auto bg-background">
-            <div class="flex p-8 max-w-2xl">
-                <div class="mr-4 max-auto block">
+            <div class="sm:flex block p-8 max-w-2xl">
+                <div class="mr-4 max-auto block sm:w-6/12 w-full mb-3">
                     <h2 class="text-center text-3xl font-bold text-secondary">KPR STT-NF 2024</h2>
                     <div class="">
                         <img src="{{ asset('img/logo.png') }}" alt="pemira_logo" width="170" class="mx-auto" />
@@ -15,14 +15,14 @@
                         ducimus deleniti dicta, deserunt molestias dolore!
                     </p>
                 </div>
-                <div class="shadow rounded-xl p-5 bg-card items-center">
+                <div class="shadow rounded-xl p-5 bg-card items-center sm:w-6/12 w-full mb-3">
                     <h1 class="text-center text-2xl mt-10">Login</h1>
                     <form method="POST" action="{{ route('auth') }}">
                         @csrf
                         <div class="mb-3">
                             <label for="nim">NIM</label>
                             <input id="nim" type="nim"
-                                class="border-2 block border-secondary p-2 rounded-lg @error('nim') is-invalid @enderror"
+                                class="w-full sm:w-12/12 border-2 block border-secondary p-2 rounded-lg @error('nim') is-invalid @enderror"
                                 name="nim" value="{{ old('nim') }}" required autocomplete="nim" autofocus>
 
                             @error('nim')
@@ -34,7 +34,7 @@
                         <div class="mb-3">
                             <label for="">Password</label>
                             <input id="password" type="password"
-                                class="border-2 block border-secondary p-2 rounded-lg @error('password') is-invalid @enderror"
+                                class="w-full sm:w-12/12 border-2 block border-secondary p-2 rounded-lg @error('password') is-invalid @enderror"
                                 name="password" required autocomplete="current-password">
 
                             @error('password')
--- resources/views/pages/transition_vote/ready_to_vote.blade.php
+++ resources/views/pages/transition_vote/ready_to_vote.blade.php
@@ -1,18 +1,6 @@
-<!DOCTYPE html>
-<html>
+@extends('layouts.vote')
 
-<head>
-    <meta charset="UTF-8" />
-    <title>Ready to Vote ? - Pemira 2024 STTNF</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.5.0/dist/full.min.css" rel="stylesheet" type="text/css" />
-</head>
-
-<body>
-    <html data-theme="cupcake">
-
-    </html>
+@section('content')
     <div class="container-full h-screen flex items-center bg-main">
         <div class="block mx-auto text-center">
             <h1 class="text-center text-white text-5xl mb-6">Sudah Siap Voting?</h1>
@@ -20,6 +8,4 @@
             <a href="/vote/bem" class="bg-primary text-white p-3 rounded-xl text-xl">Lanjut</a>
         </div>
     </div>
-</body>
-
-</html>
+@endsection
