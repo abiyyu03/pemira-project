e5d7d9564b5e0340f2b3d401340a3a8b875fa1a8
abiyyu03 <abiyyu652@gmail.com>
2024-02-19 19:32:44 +0700

    update dashboard

--- resources/views/admin/pages/dashboard.blade.php
+++ resources/views/admin/pages/dashboard.blade.php
@@ -8,7 +8,7 @@
     </header>
 
     <div class="page-heading">
-        <h3>Statistics</h3>
+        <h3>Dashboard</h3>
     </div>
     <div class="page-content">
         <section class="row">
@@ -84,7 +84,27 @@
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-12">
+                    <div class="col-4">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>Profile Visit</h4>
+                            </div>
+                            <div class="card-body">
+                                <div id="chart-profile-visit"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>Profile Visit</h4>
+                            </div>
+                            <div class="card-body">
+                                <div id="chart-profile-visit"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
                         <div class="card">
                             <div class="card-header">
                                 <h4>Profile Visit</h4>
