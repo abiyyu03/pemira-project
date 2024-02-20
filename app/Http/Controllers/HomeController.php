6604f34f9b01cf8cb3295ceae0af3e6fe274d912
abiyyu03 <abiyyu652@gmail.com>
2024-02-20 18:51:36 +0700

    add conditional when querying candidate

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,7 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $kandidat = Candidate::get();
+        $kandidat = Candidate::where('candidate_number', '!=', '0')->get();
         return view('home', compact('kandidat'));
     }
 }
