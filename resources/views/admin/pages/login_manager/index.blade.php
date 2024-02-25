5afca6e863ca083633693835829c9d6babe353af
abiyyu03 <abiyyu652@gmail.com>
2024-02-26 00:13:43 +0700

    update home

--- resources/views/admin/pages/login_manager/index.blade.php
+++ resources/views/admin/pages/login_manager/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
         integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <title>Document</title>
     <link rel="stylesheet" href="//cdn.datatables.net/2.0.0/css/dataTables.dataTables.min.css">
@@ -77,8 +77,8 @@
     <script src="//cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous">
     </script>
-    {{ $dataTable->scripts() }}
     <script src="//cdn.datatables.net/2.0.0/js/dataTables.min.js"></script>
+    {{ $dataTable->scripts() }}
     {{-- <script>
         $(function() {
             // $('#table1').DataTable({
