9efa4d650763575ad21f59b514717f1978d13b1a
abiyyu03 <abiyyu652@gmail.com>
2024-02-27 12:32:02 +0700

    update query

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -7,6 +7,7 @@ use App\Models\Candidate;
 use App\Models\User;
 use App\Models\Vote;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
@@ -19,7 +20,7 @@ class DashboardController extends Controller
     {
         $totalMahasiswa = User::where('name', '!=', 'Admin KPR')->count();
         $totalKandidat = Candidate::count();
-        $totalSuaraMasuk = Vote::groupBy('candidate_id')->count();
+        $totalSuaraMasuk = Vote::groupBy('user_id')->count(DB::raw('DISTINCT user_id'));
         $totalSisaSuara = User::where('name', '!=', 'Admin KPR')->where('status', '0')->count();
         return view('admin.pages.dashboard', compact(
             'totalMahasiswa',
